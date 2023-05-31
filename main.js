249136cae9c15ea3c9158172cd6651aa8e51db7e
Raffael2323 <118739446+Raffael2323@users.noreply.github.com>
2023-05-30 23:01:55 -0300

    update

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 
+const container = document.querySelector(".container");
+const input = document.querySelector(".input");
+const btn = document.querySelector('.btn');
 
+
+const cliente = {
+    nome: "",
+}
+btn.addEventListener('click', function () {
+    cliente.name = input.value;
+    container.innerHTML = `Bem vindo! ${cliente.name}`;
+})
+
+console.log(cliente);
